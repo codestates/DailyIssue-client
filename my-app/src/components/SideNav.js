8ca65fc680558cf0f2827c48ca1472775bba4b6c
ehdgur3125 <ehdgur3125@naver.com>
2021-03-22 17:08:51 +0900

    [Main, SideNav]/fix calender

--- my-app/src/components/SideNav.js
+++ my-app/src/components/SideNav.js
@@ -2,12 +2,28 @@ import React from "react"
 import "./SideNav.css"
 import {useState} from 'react';
 import moment from 'moment';
+import axios from 'axios';
 
-function SideNav({ hotissue }) {
+function SideNav(props) {
   const [getMoment, setMoment]=useState(moment());
   const today = getMoment;
   const firstWeek = today.clone().startOf('month').week();
   const lastWeek = today.clone().endOf('month').week() === 1 ? 53 : today.clone().endOf('month').week();
+  const handleCalenderClick=function(date){
+    props.handleDate(date);
+    axios.get(`http://15.165.161.223:4000/main/${date}`)
+    .then(data=>{
+      console.log("???");
+      props.handleIssue(data.data);
+    })
+    .catch(e=>console.log("not found dailyIssue"));
+    axios.get(`http://15.165.161.223:4000/main/hotissue/${date}`)
+    .then(data=>{
+      props.handleHotIssue(data.data.hotIssues);
+    })
+    .catch(e=>console.log("not found hotIssues"));
+  }
+
   const calendarArr=()=>{
 
     let result = [];
@@ -18,7 +34,6 @@ function SideNav({ hotissue }) {
           {
             Array(7).fill(0).map((data, index) => {
               let days = today.clone().startOf('year').week(week).startOf('week').add(index, 'day'); //d로해도되지만 직관성
-
               if(moment().format('YYYYMMDD') === days.format('YYYYMMDD')){
                 return(
                     <td key={index} style={{backgroundColor:'red'}} >
@@ -27,13 +42,13 @@ function SideNav({ hotissue }) {
                 );
               }else if(days.format('MM') !== today.format('MM')){
                 return(
-                    <td key={index} style={{backgroundColor:'gray'}} >
+                    <td key={index} style={{backgroundColor:'gray'}} onClick={()=>handleCalenderClick(days.format('YYYY-MM-DD'))}>
                       <span>{days.format('D')}</span>
                     </td>
                 );
               }else{
                 return(
-                    <td key={index}  >
+                    <td key={index} onClick={()=>handleCalenderClick(days.format('YYYY-MM-DD'))}>
                       <span>{days.format('D')}</span>
                     </td>
                 );
@@ -45,29 +60,35 @@ function SideNav({ hotissue }) {
     }
     return result;
   }
-
-return (
-  <div className="Side_nav">
-      <div className="side">
-      <div className="control">
-        <button className="button" onClick={()=>{ setMoment(getMoment.clone().subtract(1, 'month')) }}>이전달</button>
-        <span>{today.format('YYYY / MM ')}</span>
-        <button className="button" onClick={()=>{ setMoment(getMoment.clone().add(1, 'month')) }} >다음달</button>
-      </div>
-      <table>
-        <tbody>
-          {calendarArr()}
-        </tbody>
-      </table>
-  </div>
-  <div className="Hot_Issue">
-    Hot Issue
-      <ul>
-      <li className="li">first</li>
-      </ul>
-  </div>
-  <br></br>
-  </div>
+  const hotIssues=Array(Math.min(props.hotIssues.length+1,3)).fill(
+    {
+      title:props.date===today.format('YYYY-MM-DD')
+      ?'Add new Small Issue!'
+      :'No more hot issue at this day'
+    });
+  props.hotIssues.forEach((hotIssue,index)=>hotIssues[index]=hotIssue);
+  return (
+    <div className="Side_nav">
+        <div className="side">
+        <div className="control">
+          <button className="button" onClick={()=>{ setMoment(getMoment.clone().subtract(1, 'month')) }}>이전달</button>
+          <span>{today.format('YYYY / MM ')}</span>
+          <button className="button" onClick={()=>{ setMoment(getMoment.clone().add(1, 'month')) }} >다음달</button>
+        </div>
+        <table>
+          <tbody>
+            {calendarArr()}
+          </tbody>
+        </table>
+    </div>
+    <div className="Hot_Issue">
+      Hot Issues
+        <ul>
+          {hotIssues.map((hotIssue,index)=><li className="li" key={index}>{hotIssue.title}</li>)}
+        </ul>
+    </div>
+    <br></br>
+    </div>
 
 );
 
